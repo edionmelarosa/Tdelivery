93da07f8b93a829da0fc45ae746ae94bb1ab7c98
edionme <edion@ultro.co.uk>
2015-08-16 07:37:11 +0800

    fixe merge conflits

--- app/client/views/thankyou/thankyou.js
+++ app/client/views/thankyou/thankyou.js
@@ -18,19 +18,3 @@ Template.HomePublic.events({
 Template.HomePublic.helpers({
 	
 });
-
-Template.HomePublicHomeJumbotron.rendered = function() {
-	
-};
-
-Template.HomePublicHomeJumbotron.events({
-	"click #jumbotron-button": function(e, t) {
-		e.preventDefault();
-		Router.go("", {});
-	}
-	
-});
-
-Template.HomePublicHomeJumbotron.helpers({
-	
-});
