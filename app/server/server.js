421f21a10320a42c9197500d35ac99a6cb113111
Ismael 21POS <ismaelj@posbang.com>
2015-08-16 05:32:20 +0800

    updates

--- app/server/server.js
+++ app/server/server.js
@@ -114,19 +114,33 @@ Accounts.urls.resetPassword = function (token) {
 Meteor.startup( function() {
 
 	var prodCat = [
-		{prodCatid: "101", name: "Burger", desc: 'A full half pound lean ground beef patty grilled to order & topped with grilled mushrooms and onions & served on a tasted bun, layered with mayo, lettuce, tomato, and pickle', img: "http://2.bp.blogspot.com/_Uq5nkc25IJA/SfOOEBK0QBI/AAAAAAAAABA/WSbXV0n0N7w/s200/burgers.jpg"},
-		{prodCatid: "102", name: "Popcorn", desc: 'A full half pound lean ground beef patty grilled to order & topped with grilled mushrooms and onions & served on a tasted bun, layered with mayo, lettuce, tomato, and pickle', img: "http://www.moviesatthemarion.com/images/popcorn.jpg"},
-		{prodCatid: "103", name: "Drinks", desc: 'A full half pound lean ground beef patty grilled to order & topped with grilled mushrooms and onions & served on a tasted bun, layered with mayo, lettuce, tomato, and pickle', img: "http://reciclaunicel.com.mx/web/products.nsf/files/BigDrinksUK.jpg/$FILE/BigDrinksUK.jpg"},
+		{prodCatid: "101", name: "Burger", img: "http://2.bp.blogspot.com/_Uq5nkc25IJA/SfOOEBK0QBI/AAAAAAAAABA/WSbXV0n0N7w/s200/burgers.jpg"},
+		{prodCatid: "102", name: "Popcorn", img: "http://www.moviesatthemarion.com/images/popcorn.jpg"},
+		{prodCatid: "103", name: "Chips",  img: "http://smallbites.andybellatti.com/wp-content/uploads/2009/08/potato_chips.jpg"},
+		{prodCatid: "104", name: "Others",  img: "http://reciclaunicel.com.mx/web/products.nsf/files/BigDrinksUK.jpg/$FILE/BigDrinksUK.jpg"}
+	];
+
+	var prodVendor = [
+		{prodVendorid: "201", name: 'McDonalds', img: "http://lifestyle.inquirer.net/files/2013/03/mcdo.jpg" class="img-responsive product-img"},
+		{prodVendorid: "202", name: 'KFC', img: "http://brickit.com/images/clients/logo_KFC.png"},
+		{prodVendorid: "203", name: 'Chowking', img: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p160x160/306754_448189585213118_445981934_n.jpg?oh=f7dfe3766527450eaa2a7288c0842f0f&oe=567BDB03&__gda__=1447500943_c8c5d4f076537d7c43ca88dbe3c083d3"},
+		{prodVendorid: "203", name: 'Jollibee', img: "http://jollibeefoundation.org/wp-content/themes/jgf/images/jb_logo.gif"},
 	];
 
 	var products = [
-		{prodID: "12", catId: "101", cat: "", name: "name", desc: "desc", img: "imgpath", price: "58"},
-		{catId: "", name: "name", "desc", img: "imgpath", price: "58"}
-	];
+		{prodID: "001", catId: "101", vendorId: "201", name: "Big N Tasty Ala Carte", desc: "Made only from choice ingredients: 100% pure beef patty, fresh tomato, crispy lettuce, onions, pickles, creamy cheese and a special smokey sauce. It has the right mix of tangy and creamy taste and the balance of soft, crunchy and chunky textures in every bite.", img: "https://mcdonalds.com.ph/userfiles/images/ourfood/main/bnt.png", price: "137.00"},
+		{prodID: "002", catId: "101", vendorId: "201", name: "Big Mac Ala Carte", desc: "A double layer of sear-sizzled 100% pure beef mingled with special sauce on a sesame seed bun and topped with melty American cheese, crisp lettuce, minced onions and tangy pickles.", img: "http://www.mcdonalds.com/content/dam/McDonalds/item/mcdonalds-Big-Mac.png", price: "128.00"},
+		{prodID: "003", catId: "101", vendorId: "201", name: "Filet O Fish Ala Carte", desc: "Fried fish fillet smothered with sauce and topped with a slice of melted cheese in between steamed buns.", img: "https://mcdonalds.com.ph/userfiles/images/ourfood/main/filet_o_fish.png", price: "99.00"},
+		{prodID: "004", catId: "101", vendorId: "201", name: "CheeseBurger Ala Carte", desc: "100% Pure beef with real American cheese.", img: "https://mcdonalds.com.ph/userfiles/images/ourfood/main/cheeseburger.png", price: "41.00"},
 
-	var vendors = [
-		
-	];
+		{prodID: "005", catId: "101", vendorId: "202", name: "ZINGER BURGER Ala-carte", desc: "100% breast fillet chicken coated in Zinger flavouring combined with lettuce and mayo for those seeking a full on hot and spicy flavour hit.", img: "https://kfc.com.au/media/339334/burger_zinger.jpg", price: "95.00"},
+		{prodID: "006", catId: "101", vendorId: "202", name: "DOUBLE CRUNCH BURGER Ala-carte", desc: "A delicious Double Crunch Burger, with two 100% breast fillet chicken crispy strips, creamy pepper mayo, crisp lettuce on a warm sesame seed bun.", img: "https://kfc.com.au/media/339336/burger_doublecrunch.jpg", price: "95.00"},
 
-	var vend
+		{prodID: "007", catId: "101", vendorId: "204", name: "Burger Yum", desc: "Burger Yum Solo", img: "https://kfc.com.au/media/339336/burger_doublecrunch.jpg", price: "30.00"},
+		{prodID: "008", catId: "101", vendorId: "204", name: "Burger Yum with Cheese", desc: "Burger Yum with Cheese - Solo", img: "http://www.jollibee.com.ph/wp-content/uploads/2014/05/RYC-res.jpg", price: "40.00"},
+		{prodID: "010", catId: "101", vendorId: "204", name: "Burger with TLC", desc: "Burger with TLC - Solo", img: "http://www.jollibee.com.ph/wp-content/uploads/2014/07/yum-with-tlc.jpg", price: "64.00"},
+
+		{prodID: "011", catId: "104", vendorId: "203", name: "Chunky Asado Siopao", desc: "Siopao Asado can be enjoyed anytime of the day and any day of the week. You can serve this treat for breakfast, lunch , merienda , dinner or even as midnight snack", img: "https://assets.foodpanda.ph/dynamic/images/products/233/233491_1424164264_ma.jpg", price: "36.00"},
+		{prodID: "012", catId: "104", vendorId: "203", name: "Buchi", desc: "Glutinous rice balls with rich lotus filling. Deep-fried and sprinkled with sesame seeds. Buchi Lotus Cream - 2pcs", img: "http://lutongpinas.com/wp-content/uploads/2012/02/buchi.jpg", price: "39.00"},
+	];
 });
